16110720e1dafd887aa9a98605a31a16069407a0
Anass Yakkane <ayakkane@SMB.SQLI.COM>
2022-03-14 15:07:27 +0100

    bird commit

--- Animal.java
+++ Animal.java
@@ -2,11 +2,13 @@ public class Animal {
     static String printdefault(){
         return "Hello";
     }
-
+    static String birdsong(){
+        return "tweet";
+    }
     public static void main(String[] args) {
-     String output = printdefault();
-   
-
+        String output = printdefault();
+        String first = args[0];
+        if(first.equalsIgnoreCase("bird")) output = birdsong();
         System.out.println(output);
 
 
