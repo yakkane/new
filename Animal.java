9be15f3358f249cae406d2bbc5f6303dd79ad450
Anass Yakkane <ayakkane@SMB.SQLI.COM>
2022-03-14 15:12:32 +0100

    cat commit

--- Animal.java
+++ Animal.java
@@ -1,11 +1,17 @@
 public class Animal {
+
     static String printdefault(){
         return "Hello";
     }
+    static String catsound(){
+        return "meow";
+    }
 
     public static void main(String[] args) {
-     String output = printdefault();
-   
+
+        String output = printdefault();
+        String first = args[0];
+   if(first.equalsIgnoreCase("cat")) output=catsound();
 
         System.out.println(output);
 
