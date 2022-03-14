09a9efea948b5469da41bbf221a418230c76be74
Anass Yakkane <ayakkane@SMB.SQLI.COM>
2022-03-14 15:04:12 +0100

    master commit

--- Animal.java
+++ Animal.java
@@ -2,13 +2,11 @@ public class Animal {
     static String printdefault(){
         return "Hello";
     }
-    static String birdsong(){
-        return "tweet";
-    }
+
     public static void main(String[] args) {
      String output = printdefault();
-     String first = args[0];
-     if(first.equalsIgnoreCase("bird")) output = birdsong();
+   
+
         System.out.println(output);
 
 
